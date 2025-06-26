f30cdf358841230d8c5e746e51913cfff4516a74
eristocrates <reillocb@gmail.com>
2025-06-26 14:50:50 -0400

    namespace and uri  on elements and attributes

--- src/main/scala/Fs2Xfo.scala
+++ src/main/scala/Fs2Xfo.scala
@@ -3,8 +3,8 @@ import fs2.Stream
 import fs2.data.xml._
 import fs2.data.xml.XmlEvent._
 import java.io.InputStream
-import java.nio.file.Paths
 
+// === Infoset Representation ===
 sealed trait InfosetItem
 
 case class ElementInfo(
@@ -27,64 +27,84 @@ case class CharacterInfo(text: String) extends InfosetItem
 case class CommentInfo(comment: String) extends InfosetItem
 case object EndElementInfo extends InfosetItem
 
+// === QName Extension Method ===
+extension (qn: QName)
+  def formatted: String = qn.prefix.map(p => s"$p:${qn.local}").getOrElse(qn.local)
+
 object Fs2Xfo extends IOApp.Simple {
 
-  def liftEvent: XmlEvent => Stream[IO, InfosetItem] = {
+  def liftEvent(context: Map[String, String]): XmlEvent => (Stream[IO, InfosetItem], Map[String, String]) = {
     case StartTag(qn, attrs, _) =>
+      val xmlnsDecls = attrs.collect {
+        case Attr(QName(Some("xmlns"), pfx), value) =>
+          pfx -> value.collect { case XmlString(s, _) => s }.mkString
+        case Attr(QName(None, "xmlns"), value) =>
+          "" -> value.collect { case XmlString(s, _) => s }.mkString
+      }.toMap
+
+      val updatedContext = context ++ xmlnsDecls
+
+      def resolveNamespaceUri(prefix: Option[String]): Option[String] =
+        prefix.flatMap(updatedContext.get)
+
       val attrItems = attrs.map { a =>
         AttributeInfo(
-          qualifiedName = a.name.toString,
+          qualifiedName = a.name.formatted,
           localName = a.name.local,
           prefix = a.name.prefix,
-          namespaceUri = None,
+          namespaceUri = resolveNamespaceUri(a.name.prefix),
           value = a.value.collect { case XmlString(s, _) => s }.mkString
         )
       }.toList
 
-      Stream.emit(ElementInfo(
-        qualifiedName = s"${qn.prefix.map(_ + ":").getOrElse("")}${qn.local}",
-        localName = qn.local,
-        prefix = qn.prefix,
-        namespaceUri = None,
-        attributes = attrItems
-      ))
+      val elemNsUri = resolveNamespaceUri(qn.prefix)
 
-    case EndTag(_) => Stream.emit(EndElementInfo)
+      (
+        Stream.emit(ElementInfo(
+          qualifiedName = qn.formatted,
+          localName = qn.local,
+          prefix = qn.prefix,
+          namespaceUri = elemNsUri,
+          attributes = attrItems
+        )),
+        updatedContext
+      )
+
+    case EndTag(_) => (Stream.emit(EndElementInfo), context)
 
     case XmlString(text, _) if text.trim.nonEmpty =>
-      Stream.emit(CharacterInfo(text.trim))
+      (Stream.emit(CharacterInfo(text.trim)), context)
 
-    case XmlEvent.Comment(content) =>
-      Stream.emit(CommentInfo(content))
+    case Comment(content) =>
+      (Stream.emit(CommentInfo(content)), context)
 
-    case _ => Stream.empty
+    case _ => (Stream.empty, context)
   }
 
   def infosetToXml(item: InfosetItem): String = item match {
     case ElementInfo(qn, local, prefix, ns, attrs) =>
       val attrsXml = attrs.map { a =>
-        s"""  <attribute-information-item>
-  <key>
-    <qualified-name>${a.qualifiedName}</qualified-name>
-    <local-name>${a.localName}</local-name>
-    <prefix>${a.prefix.getOrElse("")}</prefix>
-    <namespace-uri>${a.namespaceUri.getOrElse("")}</namespace-uri>
-  </key>
-  <value>${a.value}</value>
-</attribute-information-item>"""
+        s"""<attribute-information-item>
+           |  <key>
+           |    <qualified-name>${a.qualifiedName}</qualified-name>
+           |    <local-name>${a.localName}</local-name>
+           |    <prefix>${a.prefix.getOrElse("")}</prefix>
+           |    <namespace-uri>${a.namespaceUri.getOrElse("")}</namespace-uri>
+           |  </key>
+           |  <value>${a.value}</value>
+           |</attribute-information-item>""".stripMargin
       }.mkString("\n")
 
       s"""<element-information-item>
-  <qualified-name>$qn</qualified-name>
-  <local-name>$local</local-name>
-  <prefix>${prefix.getOrElse("")}</prefix>
-  <namespace-uri>${ns.getOrElse("")}</namespace-uri>
-  <attributes>
-$attrsXml
-  </attributes>
-</element-information-item>"""
+         |  <qualified-name>$qn</qualified-name>
+         |  <local-name>$local</local-name>
+         |  <prefix>${prefix.getOrElse("")}</prefix>
+         |  <namespace-uri>${ns.getOrElse("")}</namespace-uri>
+         |  <attributes>
+         |$attrsXml
+         |  </attributes>""".stripMargin
 
-    case EndElementInfo => "<!-- End of element-information-item -->"
+    case EndElementInfo => "</element-information-item>"
 
     case CharacterInfo(text) =>
       s"<character-information-item>$text</character-information-item>"
@@ -102,16 +122,22 @@ $attrsXml
           .through(fs2.text.utf8.decode)
           .through(events[IO, String]())
 
-      val infoset = xmlEvents.flatMap(liftEvent)
+      val infosetStream = xmlEvents
+        .evalScan((Stream.empty.covaryAll[IO, InfosetItem], Map.empty[String, String])) {
+          case ((_, ctx), event) =>
+            val (out, newCtx) = liftEvent(ctx)(event)
+            IO.pure((out, newCtx))
+        }
+        .flatMap(_._1)
 
       val xmlOutput = Stream.emit("<document-information-item>") ++
-        infoset.map(infosetToXml) ++
+        infosetStream.map(infosetToXml) ++
         Stream.emit("</document-information-item>")
 
       xmlOutput
         .intersperse("\n")
         .through(fs2.text.utf8.encode)
-        .through(fs2.io.file.Files[IO].writeAll(Paths.get("xfo_output.xml")))
+        .through(fs2.io.file.Files[IO].writeAll(java.nio.file.Paths.get("xfo_output.xml")))
         .compile
         .drain
     }
